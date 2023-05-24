788f3a18f5f3518592fb5ce1453b7ae5c4e57a80
Nhunor06 <61747377+Nhunor06@users.noreply.github.com>
2023-05-24 10:20:11 +0200

    2. Beolvasás kész

---
+++ Operatorok/Operatorok.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Operatorok
+{
+    internal class Operatorok
+    {
+        string sor;
+        int elsoElem;
+        string operatorJel;
+        int masodikElem;
+        public Operatorok(string sor)
+        {
+            this.sor = sor;
+            var felbont = sor.Split(' ');
+            this.elsoElem = int.Parse(felbont[0]);
+            this.operatorJel = felbont[1];
+            this.masodikElem = int.Parse(felbont[2]);
+
+        }
+
+        public string Sor { get => sor; }
+        public int ElsoElem { get => elsoElem; }
+        public string OperatorJel { get => operatorJel; }
+        public int MasodikElem { get => masodikElem; }
+
+    }
+}
--- Operatorok/Program.cs
+++ Operatorok/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Operatorok
 {
     internal class Program
     {
+        string fileLocation = "Sources\\kifejezesek.txt";
         static void Main(string[] args)
         {
+            List<> = new List<Operatorok>;
             Console.WriteLine("Hello World!");
         }
     }
